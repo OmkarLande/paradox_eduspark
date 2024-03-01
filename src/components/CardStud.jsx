b9b66ad7b48259d9035268606c6938010b62650c
Siddhesh <siddheshwayal114@gmail.com>
2024-03-01 19:28:54 +0530

    done

--- src/components/CardStud.jsx
+++ src/components/CardStud.jsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React,{useState} from "react";
 import { Link } from "react-router-dom";
 
 const CardStud = (props) => {
-
+const [isHovered, setIsHovered] = useState(false);
   const name = props.name
   const desc = props.desc
   const roomId = props.roomId
+   
+
+  const defaultStyle = {
+    transition: 'transform 0.3s ease-out',
+    transform: isHovered ? 'scale(1.1)' : 'scale(1)', // Apply pop-out effect only on hover
+  };
   function getRandomColor() {
     const colors = [
       "#FF5733",
@@ -20,15 +26,18 @@ const CardStud = (props) => {
 }
   
   return (
-    <div className="flex flex-row flex-wrap" >
+    <div className="flex flex-row flex-wrap " >
       <Link to={`/StudRoom/${roomId}`}>
         
         <div
             className="card w-72 h-72 m-2 shadow-xl "
-            style={{"backgroundColor":getRandomColor(),}}
+            
         >
+          <div style={{"backgroundColor":getRandomColor(),}} className='h-56 rounded-t-xl'>
+
+          </div>
           <div className="card-body flex flex-col-reverse items-center  p-2 ">
-            <div className="mb-5 flex flex-col items-center text-white">
+            <div className="mb-5 flex flex-col items-center text-black">
               <h2 className="card-title  font-Grish text-3xl">{name} </h2>
                 <p className="text-xl font-sans">{desc}</p>
 
